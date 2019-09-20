f19f246dfa4356ddeee0f4effb6ea6adce1dfc79
Khalid <khalid@bawes.net>
2019-09-20 05:43:35 +0300

    Add api env

---
+++ api/config/main.php
@@ -0,0 +1,60 @@
+<?php
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
+return [
+    'id' => 'app-api',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'api\controllers',
+    'bootstrap' => ['log'],
+    'modules' => [
+        'v1' => [
+            'class' => 'api\modules\v1\Module',
+        ],
+    ],
+    'components' => [
+        'request' => [
+            'csrfParam' => '_csrf-api',
+            // Accept and parse JSON Requests
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'enableStrictParsing' => true,
+            'showScriptName' => false,
+            'rules' => [
+                [ // AccountController
+                    'class' => 'yii\rest\UrlRule',
+                    'controller' => 'v1/account',
+                    'pluralize' => false,
+                    'patterns' => [
+                        'GET salary' => 'salary',
+                        'GET employer' => 'employer',
+                        'POST change-password' => 'change-password',
+                        // OPTIONS VERBS
+                        'OPTIONS' => 'options',
+                        'OPTIONS salary' => 'options',
+                        'OPTIONS employer' => 'options',
+                        'OPTIONS change-password' => 'options'
+                    ]
+                ],
+            ],
+        ],
+    ],
+    'params' => $params,
+];
---
+++ api/modules/v1/controllers/BotController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use Yii;
+use yii\rest\Controller;
+use yii\filters\Cors;
+
+/**
+ * Base Bot Controller
+ */
+class BotController extends Controller
+{
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        // Allow XHR Requests from our different subdomains and dev machines
+        $behaviors['corsFilter'] = [
+            'class' => Cors::className(),
+            'cors' => [
+                'Origin' => Yii::$app->params['allowedOrigins'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Allow-Credentials' => null,
+                'Access-Control-Max-Age' => 86400,
+                'Access-Control-Expose-Headers' => [
+                    'X-Pagination-Current-Page',
+                    'X-Pagination-Page-Count',
+                    'X-Pagination-Per-Page',
+                    'X-Pagination-Total-Count'
+                ],
+            ],
+        ];
+
+        return $behaviors;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        $actions = parent::actions();
+        $actions['options'] = [
+            'class' => 'yii\rest\OptionsAction',
+            // optional:
+            'collectionOptions' => ['GET', 'POST', 'HEAD', 'OPTIONS'],
+            'resourceOptions' => ['GET', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+        ];
+        return $actions;
+    }
+
+
+    public function actionChangePassword()
+    {
+        $oldPassword = Yii::$app->request->getBodyParam("old_password");
+        $newPassword = Yii::$app->request->getBodyParam("new_password");
+
+        return [
+            "operation" => "error",
+            "message" => "Empty old password"
+        ];
+    }
+}
