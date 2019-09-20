01105445c33e7fc41d9ab0dbc3536cc136e4809b
Khalid <khalid@bawes.net>
2019-09-20 05:44:35 +0300

    Add db config

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=localhost;dbname=smschatbot',
+            'username' => 'chatbotuser',
+            'password' => 'chatbotpass',
             'charset' => 'utf8',
         ],
         'mailer' => [
