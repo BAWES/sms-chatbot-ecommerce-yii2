ec1f2de77698c9bc95c8f4d576575487cbd6a8ca
Khalid <khalid@bawes.net>
2020-01-03 21:46:10 +0300

    Remove update btn

--- backend/views/user/view.php
+++ backend/views/user/view.php
@@ -16,7 +16,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->phone], ['class' => 'btn btn-primary']) ?>
         <?= Html::a('Delete', ['delete', 'id' => $model->phone], [
             'class' => 'btn btn-danger',
             'data' => [
